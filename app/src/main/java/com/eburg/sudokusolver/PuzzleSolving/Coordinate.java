746a9c252e3e869ddca616ed05685bddd07b74dd
Robert Clarke <clarke.john.robert@gmail.com>
2019-11-18 16:49:10 -0500

    Finished preliminary SudokuBoard methods, still needs to be able to set default state

---
+++ app/src/main/java/com/eburg/sudokusolver/PuzzleSolving/Coordinate.java
@@ -0,0 +1,11 @@
+package com.eburg.sudokusolver.PuzzleSolving;
+
+public class Coordinate {
+    public int x;
+    public int y;
+
+    public Coordinate(int xVal, int yVal) {
+        x = xVal;
+        y = yVal;
+    }
+}

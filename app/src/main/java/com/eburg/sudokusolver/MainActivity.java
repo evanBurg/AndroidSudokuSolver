624eb3fd8c86923fb381844983c44fd3520eb7c6
Evan Burgess <burgenator@hotmail.com>
2019-11-18 14:37:45 -0500

    Add ML Kit code

--- app/src/main/java/com/eburg/sudokusolver/MainActivity.java
+++ app/src/main/java/com/eburg/sudokusolver/MainActivity.java
@@ -1,7 +1,18 @@
 package com.eburg.sudokusolver;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
 import android.os.Bundle;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +20,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    /*
+        Usage: getImageText(image)
+               .addOnSuccessListener(firebaseVisionText -> {
+                   firebaseVisionText.getText();
+                })
+              .addOnFailureListener(exception -> {
+                   txtView.setText("task failed");
+               });
+     */
+    private Task<FirebaseVisionText> getImageText(Bitmap image){
+        FirebaseVisionImage vImage = FirebaseVisionImage.fromBitmap(image);
+        FirebaseVisionTextRecognizer detector = FirebaseVision.getInstance().getOnDeviceTextRecognizer();
+        return detector.processImage(vImage);
     }
 }

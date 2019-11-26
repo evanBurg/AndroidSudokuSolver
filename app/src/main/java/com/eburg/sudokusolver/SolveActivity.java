7cfe0792583084c41940096cb08f38f054c420ae
Evan Burgess <burgenator@hotmail.com>
2019-11-26 16:39:49 -0500

    Finish the sudoku board correction view

--- app/src/main/java/com/eburg/sudokusolver/SolveActivity.java
+++ app/src/main/java/com/eburg/sudokusolver/SolveActivity.java
@@ -1,70 +1,181 @@
 package com.eburg.sudokusolver;
 
+import android.animation.TimeInterpolator;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.text.InputFilter;
 import android.text.InputType;
+import android.transition.ChangeBounds;
+import android.transition.TransitionManager;
+import android.transition.TransitionSet;
 import android.view.Gravity;
 import android.view.View;
-import android.widget.Button;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import com.github.dhaval2404.imagepicker.ImagePicker;
+import com.github.ybq.android.spinkit.SpinKitView;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
 import com.google.firebase.ml.vision.FirebaseVision;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.text.FirebaseVisionText;
 import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+import com.takusemba.spotlight.OnTargetListener;
+import com.takusemba.spotlight.Spotlight;
+import com.takusemba.spotlight.Target;
+import com.takusemba.spotlight.effet.RippleEffect;
+import com.takusemba.spotlight.shape.Circle;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+import static android.graphics.Color.argb;
 
 public class SolveActivity extends AppCompatActivity {
     public static final int GET_FROM_GALLERY = 3;
     private Bitmap LOADED_IMAGE = null;
     private TextView output;
+    private CardView cardView;
+    private RelativeLayout solveContainer;
+    private boolean isCollapsed = true;
     private ImageResults lastResults = null;
     private ImageView showImage;
+    private TextView showImageText;
+    private TextView chevron;
     private LinearLayout inputBoardContainer;
     private ArrayList<ArrayList<EditText>> inputBoard;
+    private LinearLayout parseContainer;
+    private SpinKitView loader;
+    private Animation rotateDown;
+    private Animation rotateUp;
+    private static final int BOARD_START = 0;
+    private static final int BOARD_END = 9;
+    private static final int originalDimension = 0;
+    private static final int newDimension = LinearLayout.LayoutParams.WRAP_CONTENT;
+    private Target spotlightTarget;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.solve_activity);
-        output = findViewById(R.id.outputBoard);
-        showImage = findViewById(R.id.providedImage);
-        inputBoardContainer = findViewById(R.id.inputBoardContainer);
-        inputBoard = new ArrayList<>();
         ImagePicker.Companion.with(this).crop().start(GET_FROM_GALLERY);
 
-        for(int i = 0; i < 9; i++){
+        setContentView(R.layout.solve_activity);
+        showImage = findViewById(R.id.providedImage);
+        chevron = findViewById(R.id.chevron);
+        solveContainer = findViewById(R.id.solveContainer);
+        showImageText = findViewById(R.id.showImageText);
+        inputBoardContainer = findViewById(R.id.inputBoardContainer);
+        parseContainer = findViewById(R.id.parseContainer);
+        parseContainer.setVisibility(View.INVISIBLE);
+        loader = findViewById(R.id.loader);
+        inputBoard = new ArrayList<>();
+        cardView = findViewById(R.id.imageContainer);
+        rotateDown = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_chevron_down);
+        rotateUp = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_chevron_up);
+        ViewGroup.LayoutParams params = showImage.getLayoutParams();
+        params.height = originalDimension;
+        showImage.setLayoutParams(params);
+
+        for(int i = BOARD_START; i < BOARD_END; i++){
             LinearLayout row = new LinearLayout(this);
             inputBoard.add(new ArrayList<EditText>());
-            for(int j = 0; j < 9; j++){
+            for(int j = BOARD_START; j < BOARD_END; j++){
                 EditText text = new EditText(this);
                 text.setLayoutParams(new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT, 1.0f));
                 text.setInputType(InputType.TYPE_CLASS_NUMBER);
                 text.setGravity(Gravity.CENTER_HORIZONTAL);
-                int maxLength = 3;
+                int maxLength = 1;
                 text.setFilters(new InputFilter[] {new InputFilter.LengthFilter(maxLength)});
+                text.setBackgroundResource(getBorder(j, i));
                 row.addView(text);
                 inputBoard.get(i).add(text);
             }
             inputBoardContainer.addView(row);
         }
+    }
+
+    public void onImageClick(View v) {
+        if(isCollapsed){
+            TransitionManager.beginDelayedTransition(cardView, new TransitionSet()
+                    .addTransition(new ChangeBounds()));
+
+            chevron.startAnimation(rotateDown);
+            showImageText.setText(R.string.hideImage);
+            ViewGroup.LayoutParams params = showImage.getLayoutParams();
+            params.height = newDimension;
+            showImage.setLayoutParams(params);
+
+            isCollapsed = false;
+        } else {
+            TransitionManager.beginDelayedTransition(cardView, new TransitionSet()
+                    .addTransition(new ChangeBounds()));
+
+            chevron.startAnimation(rotateUp);
+
+            showImageText.setText(R.string.showImage);
+            ViewGroup.LayoutParams params = showImage.getLayoutParams();
+            params.height = originalDimension;
+            showImage.setLayoutParams(params);
+
+            isCollapsed = true;
+        }
+    }
+
+    private int getBorder(int x, int y){
+
+        if(((x + 1) % 3 == 0 && x != (BOARD_END - 1)) && (y + 1) % 3 == 0 && y != (BOARD_END - 1)){
+            return R.drawable.edit_text_border_both_bold;
+        }
+
+        if(x == BOARD_START && (y + 1) % 3 == 0 && y != (BOARD_END - 1)){
+            return R.drawable.edit_text_border_first_end_bold;
+        }
+
+        if((y + 1) % 3 == 0 && y != (BOARD_END - 1)){
+            return R.drawable.edit_text_border_end_bold;
+        }
+
+        if(y == (BOARD_END - 1) && (x + 1) % 3 == 0 && x != (BOARD_END - 1)){
+            return R.drawable.edit_text_border_bottom_bold;
+        }
+
+        if((x + 1) % 3 == 0 && x != (BOARD_END - 1)){
+            return R.drawable.edit_text_border_bold;
+        }
+
+        if(x == BOARD_START && y == (BOARD_END - 1)){
+            return R.drawable.edit_text_border_bottom_start;
+        }
+
+        if(y == (BOARD_END - 1)){
+            return R.drawable.edit_text_border_bottom;
+        }
+
+        if(x == BOARD_START){
+            return R.drawable.edit_text_border_start;
+        }
+
+        return R.drawable.edit_text_border;
     }
 
     @Override
@@ -121,34 +232,28 @@ public class SolveActivity extends AppCompatActivity {
 
                     lastResults = new ImageResults(characters, resultText);
                     ArrayList<ArrayList<Integer>> board = lastResults.get2DArray();
-                    String boardString = "";
                     for(int i = 0; i < board.size(); i++){
                         ArrayList<Integer> row = board.get(i);
                         for(int j = 0; j < row.size(); j++){
                             Integer num = row.get(j);
-                            boardString += num + "    ";
-
                             try{
-                               inputBoard.get(i).get(j).setText(String.valueOf(num));
+                               inputBoard.get(i).get(j).setText(num != 0 ? String.valueOf(num) : "");
                             }catch (Exception e){
                                 String what = e.getMessage();
                                 String className = e.toString();
                             }
-
                         }
-                        boardString += "\n";
                     }
 
-                    output.setText(boardString);
                     showImage.setImageBitmap(LOADED_IMAGE);
                     showImage.setVisibility(View.VISIBLE);
+                    parseContainer.setVisibility(View.VISIBLE);
+                    loader.setVisibility(View.INVISIBLE);
                     showImage.setAdjustViewBounds(true);
-
-                    //Calc
                 })
-                        .addOnFailureListener(exception -> {
-                            output.setText("Could Not Load Image");
-                        });
+                .addOnFailureListener(exception -> {
+                    output.setText("Could Not Load Image");
+                });
             } catch (FileNotFoundException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();

a122940dce9f3cf753f3a18ef68ab5d8396308e5
Robert Clarke <clarke.john.robert@gmail.com>
2019-12-02 14:07:09 -0500

    Refactored error detection code

--- app/src/main/java/com/eburg/sudokusolver/PuzzleSolving/ErrorFinder.java
+++ app/src/main/java/com/eburg/sudokusolver/PuzzleSolving/ErrorFinder.java
@@ -1,43 +1,17 @@
 package com.eburg.sudokusolver.PuzzleSolving;
 
-import android.util.Pair;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedHashSet;
 
 
-public class SudokuBoard {
-
-    public ArrayList<ArrayList<Integer>> board;
-    public ArrayList<Coordinate> canonicalPositions;
-
-    public SudokuBoard() {
-        board = new ArrayList<ArrayList<Integer>>();
-        canonicalPositions = new ArrayList<>();
-        for (int x = 0; x < 9; x++) {
-            board.add(new ArrayList<Integer>(Arrays.asList(0,0,0,0,0,0,0,0,0)));
-        }
-        Coordinate c = new Coordinate(2,3);
-        c.x = 9;
-        setCellValue(c, 9);
-    }
-
-    public void addInitialValues(ArrayList<Pair<Coordinate, Integer>> correct) {
-        for (Pair<Coordinate, Integer> pair : correct) {
-            canonicalPositions.add(pair.first);
-            setCellValue(pair.first, pair.second);
-        }
-    }
-
-    public void setCellValue(Coordinate c, Integer value) { board.get(c.y).set(c.x, value); }
-    public void getCellValue(Coordinate c) { board.get(c.y).get(c.x); }
-
-    public ArrayList<Coordinate> getErrorCoordsForRow(int yVal) {
+public class ErrorFinder {
+    public static ArrayList<Coordinate> getErrorCoordsForRow(@NotNull ArrayList<ArrayList<Integer>> board, int yVal) {
         ArrayList<Coordinate> errorCoords = new ArrayList<>();
         ArrayList<Integer> row = board.get(yVal);
-        
+
         for (int i = 1; i <= 9; i++) {
             int occurrence = Collections.frequency(row, i);
             if (occurrence > 1) { //if there is more than one occurrence in the same row, it's an error
@@ -49,7 +23,7 @@ public class SudokuBoard {
         return errorCoords;
     }
 
-    public ArrayList<Coordinate> getErrorCoordsForColumn(int xVal) {
+    public static ArrayList<Coordinate> getErrorCoordsForColumn(@NotNull ArrayList<ArrayList<Integer>> board, int xVal) {
         ArrayList<Coordinate> errorCoords = new ArrayList<>();
         ArrayList<Integer> virtualCol = new ArrayList<>();
         for (int y = 0; y < 9; y++) //flatten the column into a 1d vector
@@ -63,11 +37,10 @@ public class SudokuBoard {
                         errorCoords.add(new Coordinate(xVal, y));
             }
         }
-
         return errorCoords;
     }
 
-    private ArrayList<Coordinate> getErrorCoordsForBlockByOffset(int baseX, int baseY) {
+    private static ArrayList<Coordinate> getErrorCoordsForBlockByOffset(@NotNull ArrayList<ArrayList<Integer>> board, int baseX, int baseY) {
         ArrayList<Coordinate> errorCoords = new ArrayList<>();
         ArrayList<Integer> virtualBlock = new ArrayList<>();
         for (int yOffset = baseY; yOffset < baseY+3; yOffset++) { //flatten block into a 1d vector
@@ -75,29 +48,29 @@ public class SudokuBoard {
                 virtualBlock.add(board.get(yOffset).get(xOffset));
         }
 
-        for (int i = 1; i <= 9; i++) {
+        for (int i = 1; i <= 9; i++) { //check each number 1-9
             int occurrence = Collections.frequency(virtualBlock, i);
-            for (int p = 0; p < 9; p++)
-                if (virtualBlock.get(p) == i)
+            for (int p = 0; p < 9; p++) //examine each position in the flattened block
+                if (occurrence > 1 && virtualBlock.get(p) == i) //if there is more than one of this number, add it's coordinate to the output
                     errorCoords.add(new Coordinate(baseX + (p % 3), baseY + (p / 3)));
         }
         return errorCoords;
     }
-    public ArrayList<Coordinate> getErrorCoordsForBlock(int blockNum) {
+
+    public static ArrayList<Coordinate> getErrorCoordsForBlock(@NotNull ArrayList<ArrayList<Integer>> board, int blockNum) {
         int yBase = 3 * (blockNum / 3);
         int xBase = 3 * (blockNum % 3);
 
-        return getErrorCoordsForBlockByOffset(xBase, yBase);
+        return getErrorCoordsForBlockByOffset(board, xBase, yBase);
     }
 
-    public ArrayList<Coordinate> getAllErrorCoords() {
-
+    public static ArrayList<Coordinate> getAllErrorCoords(@NotNull ArrayList<ArrayList<Integer>> board, @NotNull ArrayList<Coordinate> canonicalPositions) {
         LinkedHashSet<Coordinate> uniqueCoords = new LinkedHashSet<Coordinate>();
 
         for (int q = 0; q < 9; q++) {
-            uniqueCoords.addAll(getErrorCoordsForRow(q));
-            uniqueCoords.addAll(getErrorCoordsForColumn(q));
-            uniqueCoords.addAll(getErrorCoordsForBlock(q));
+            uniqueCoords.addAll(getErrorCoordsForRow(board, q));
+            uniqueCoords.addAll(getErrorCoordsForColumn(board, q));
+            uniqueCoords.addAll(getErrorCoordsForBlock(board, q));
         }
 
         ArrayList<Coordinate> errorCoords = new ArrayList<>(uniqueCoords);

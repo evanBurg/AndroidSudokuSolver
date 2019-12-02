c28edf1505825e4a99843eee7741de67d457bdb3
Evan Burgess <burgenator@hotmail.com>
2019-12-01 21:52:00 -0500

    Switch from home page buttons to tabs

--- app/src/main/java/com/eburg/sudokusolver/MainActivity.java
+++ app/src/main/java/com/eburg/sudokusolver/MainActivity.java
@@ -4,24 +4,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.FirebaseApp;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FirebaseApp.initializeApp(this);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.navigation);
+        bottomNavigationView.setSelectedItemId(R.id.action_main);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this::onNavigationItemSelected);
     }
 
-    public void startSolve(View v){
+    public void startSolve() {
         startActivity(new Intent(this, SolveActivity.class));
     }
 
-    public void viewHistory(View v) {
+    public void viewHistory() {
         startActivity(new Intent(this, ListActivity.class));
     }
 
@@ -39,5 +47,29 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(this, AboutActivity.class));
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        try {
+            switch (menuItem.getItemId()) {
+                case R.id.action_history:
+                    viewHistory();
+                    break;
+                case R.id.action_main:
+                    break;
+                case R.id.action_solve:
+                    startSolve();
+                    break;
+                default:
+                    return false;
+            }
+
+            return true;
+
+        } catch (Exception e) {
+            String what = e.getMessage();
+            return false;
+        }
     }
 }
